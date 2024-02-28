fa13ab7d0fafa2a4de4eb6a4c071eedba368b709
Bernie-ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-02-28 17:28:59 -0500

    working postgres + flask

--- api/api.py
+++ api/api.py
@@ -1,5 +1,7 @@
-from flask import Flask, request,jsonify
-
+from flask import Flask,jsonify,render_template
+import requests
+import os
+import psycopg2
 import logging
 
 
@@ -9,6 +11,58 @@ app = Flask(__name__)
 
 
 
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='test',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
+
+
 @app.route('/')
-def home():
-	return {'message':'container b - flask app - route /'}
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify({'books': books})
+
+@app.route('/check_for_vehicle' ,methods=['POST'])
+def check_for_vehicle():
+
+	"""
+	to simulate api requst from react front end
+	put query params in the url
+	"""
+	#check if veh submitted on form is in db
+	#if in db, return data to front end
+	#if not in db perform scrape
+
+	#get params of request
+	make = request.args.get('make')
+	model = request.args.get('model')
+	year = request.args.get('year')
+
+	data = DB_execute_queries
+
+
+	
+
+
+# @app.route('/scrape_api/scrape', methods=['GET'])
+# def trigger_scrape():
+	
+# 	requests.post('http://127.0.0.1:7777/scrape')
+	
+
+# @app.route('/scrape_api/scraped_data',methods=['GET','POST'])
+# def scrape_result():
+
+# 	data = request.json
+# 	print(data)
+
+
+
+
