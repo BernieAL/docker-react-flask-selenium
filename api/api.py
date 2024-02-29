1beede11d421dec0ffddf2982d53b51a22fa2b85
Bernie-ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-02-29 14:31:47 -0500

    gunicorn working - serving flask app

--- api/api.py
+++ api/api.py
@@ -4,6 +4,9 @@ import os
 import psycopg2
 import logging
 
+from dotenv import load_dotenv
+load_dotenv
+
 
 logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
